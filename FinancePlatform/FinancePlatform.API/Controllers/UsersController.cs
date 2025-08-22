dacea270cd31442d0102d6b79ddc636d0a22b8e3
Lazzat Mukhametalinova <Lazzat.Mukhametalinova@kaspi.kz>
2025-08-22 13:54:39 +0500

    add connection string

--- FinancePlatform/FinancePlatform.API/Controllers/UsersController.cs
+++ FinancePlatform/FinancePlatform.API/Controllers/UsersController.cs
@@ -1,11 +1,13 @@
 using FinancePlatform.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
+using Npgsql;
 
 namespace FinancePlatform.API.Controllers;
 
 [ApiController]
 [Route("api/[controller]")]
 public class UsersController(
+    NpgsqlConnection connection,
     IUserService _userService
 ) : ControllerBase
 {
